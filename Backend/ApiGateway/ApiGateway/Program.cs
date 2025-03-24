e77ecb6fa86c802e0161dcf9ab18c55a24ea372f
ilya1004 <ilyarabets@gmail.com>
2025-03-24 16:38:05 +0300

    [ADD] Added HealthChecks and Dockerfile to ApiGateway

--- Backend/ApiGateway/ApiGateway/Program.cs
+++ Backend/ApiGateway/ApiGateway/Program.cs
@@ -6,9 +6,13 @@ var services = builder.Services;
 services.AddAuthConfiguration(builder.Configuration);
 services.AddYarpConfiguration(builder.Configuration);
 
+services.AddHealthChecks();
+
 var app = builder.Build();
 
 app.UseRateLimiter();
+
+app.MapHealthChecks("health");
 
 app.UseAuthentication();
 app.UseAuthorization();
